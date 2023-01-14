0f0bef2abc6cefccf296c8d453b0ff1900ba010a
auliantya <117278553+auliantya@users.noreply.github.com>
2023-01-14 18:11:05 +0800

    Update model.R

--- model.R
+++ model.R
@@ -1,31 +1,19 @@
 ####################################
-# Data Professor                   #
-# http://youtube.com/dataprofessor #
-# http://github.com/dataprofessor  #
+# Solar Forecasting                #
 ####################################
 
 # Importing libraries
-library(RCurl) # for downloading the iris CSV file
-library(randomForest)
+library(RCurl) # for downloading CSV file
+library(e1071)
 library(caret)
+library(data.table)
 
 # Importing the Iris data set
-iris <- read.csv(text = getURL("https://raw.githubusercontent.com/dataprofessor/data/master/iris.csv") )
-
-# Performs stratified random split of the data set
-TrainingIndex <- createDataPartition(iris$Species, p=0.8, list = FALSE)
-TrainingSet <- iris[TrainingIndex,] # Training Set
-TestingSet <- iris[-TrainingIndex,] # Test Set
-
-write.csv(TrainingSet, "training.csv")
-write.csv(TestingSet, "testing.csv")
-
-TrainSet <- read.csv("training.csv", header = TRUE)
+TrainSet <- read.csv(text = getURL("https://raw.githubusercontent.com/auliantya/solarprediction/master/training.csv"),header = TRUE)
 TrainSet <- TrainSet[,-1]
 
-# Building Random forest model
-
-model <- randomForest(Species ~ ., data = TrainSet, ntree = 500, mtry = 4, importance = TRUE)
+# Building SVM model
+model <- svm(solar_radiation ~ ., data = TrainSet, kernel = "linear", cost = 10, scale=TRUE)
 
 # Save model to RDS file
 saveRDS(model, "model.rds")
